91da3910374c47107ec1646f012c3655cb42378c
Eugene Ivantsov <eivantsov@codenvy.com>
2016-10-04 09:24:08 +0000

    Changes to Yeoman sample

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name app1App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the app1App
+ */
+angular.module('app1App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
