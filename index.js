27d1947735703c3cf839fdc11aa083d2ecad87ac
David <davidkwan95@gmail.com>
2017-01-15 15:28:49 +0000

    lalalala

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ app.post('/webhook/', function (req, res) {
         let sender = event.sender.id
         if (event.message && event.message.text) {
             let text = event.message.text
+            console.log(text);
             responser.sendTextMessage(sender, "For now, I will always response with Hello")
         }
     }
